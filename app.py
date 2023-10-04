694ac81e47014ae0f3b3d04b7f68459f0b2f4202
tarikjaber <tariknjaber@gmail.com>
2023-10-04 13:23:47 -0400

    Add dynamic route search path

--- app.py
+++ app.py
@@ -11,23 +11,38 @@ es = Elasticsearch(['https://localhost:9200'],
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    output = ""
-    # Define your search query
+def query_title(series_title):
     query_body = {
         "query": {
             "match": {
-                "Series_Title": "Star Wars"
+                "Series_Title": series_title
             }
         }
     }
 
     # Execute the search query
     response = es.search(index=INDEX_NAME, body=query_body)
+    return response['hits']['hits']
+
+@app.route('/')
+def hello_world():
+    output = ""
+
+    response = query_title("Star Wars")
 
     # Print the results
-    for hit in response['hits']['hits']:
+    for hit in response:
+        output +="<p>" +  hit['_source']["Series_Title"] + "</p>"
+        print(hit['_source']["Series_Title"])
+    return output 
+
+@app.route('/<query>')
+def search_title(query):
+    output = ""
+    response = query_title(query)
+
+    # Print the results
+    for hit in response:
         output +="<p>" +  hit['_source']["Series_Title"] + "</p>"
         print(hit['_source']["Series_Title"])
     return output 
